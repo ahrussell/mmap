e2044d7fe18ea57e29a2f108eba8dcee62bf634c
Griaffragi <Griaffragi@Griaffragi.(none)>
2014-08-25 14:34:14 -0700

    Linear Combination finished.

--- attack.py
+++ attack.py
@@ -1,16 +1,54 @@
 import clt
+import time
+
+current_time = lambda:time.time()
+
+def linearcomb(omega, x0):
+		n = len(omega)
+		matrix = block_matrix([[identity_matrix(n), matrix(omega).transpose()],[matrix(ZZ, 1, n, 0), x0]])
+		print matrix
+		while True:
+			temp = list(matrix.column(n))
+			index = next((i for i, x in enumerate(temp) if x), None)
+			minimum = temp[index]
+			count = 0
+			for i in range (self.n+1):
+				if (temp[i] != 0):
+					count+=1
+				 	if(temp[i] < minimum):
+						minimum = temp[i]
+						index = i
+			if count == 1:
+				break
+			for i in range (n+1):
+				if (i != index):
+					self.matrix.add_multiple_of_row(i, index, -(matrix[i, n]//minimum))
+			temp = list(matrix.column(n))
+		matrix = matrix.delete_rows([index, n])
+		matrix = matrix.delete_columns([n])
+		matrix = matrix.LLL()
+		print "Reduced Matrix"
+		print matrix
+		return 0
 
 def main():
-    lam = 20
-    k_atk = 5
-    print "CLT (lambda="+str(lam)+", k_atk="+str(k_atk)+")"
-    params = clt.CLT.set_params(lam, k_atk)
-    mmap = clt.CLT(params)
-    num_encodings = 10
-    mmap.run(k_atk, of_zero = True)
+	lam = 20
+	k = 5
+	print "CLT (lambda="+str(lam)+", k="+str(k)+")"
+	params = clt.CLT.set_params(lam, k)
+	mmap = clt.CLT(params)
+	num_encodings = 10
+	omega = vector(ZZ, [0 for i in range(num_encodings)])
+	for i in range(self.n):
+		self.omega[i] = (encodings[i])
+	mmap.run(k, of_zero = True)
+   	c = current_time()
+	print "Computing matrix reduction:"
+	Attack.__linearcomb__(atk)
+	print "Time:", current_time()-c
 
 if __name__=="__main__":
-    main()
+	main()
 
 # attack psuedocode
 
